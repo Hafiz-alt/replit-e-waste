b408e78a90452685ca770157107ab7e0b287f9d3
hafiz md <38473175-bca-178@users.noreply.replit.com>
2025-02-09 11:57:47 +0000

    Restored to 'e143105bc0e42b3ab904853a411f28f9dfe7c60d'
    Replit-Restored-To:e143105bc0e42b3ab904853a411f28f9dfe7c60d

--- server/auth.ts
+++ server/auth.ts
@@ -24,7 +24,6 @@ async function hashPassword(password: string) {
 async function comparePasswords(supplied: string, stored: string) {
   try {
     const [hashed, salt] = stored.split(".");
-    if (!hashed || !salt) return false;
     const hashedBuf = Buffer.from(hashed, "hex");
     const suppliedBuf = (await scryptAsync(supplied, salt, 64)) as Buffer;
     return timingSafeEqual(hashedBuf, suppliedBuf);
